093951b1717047cf4bd96f46620f0c8efbd930a8
Armin Kuburas <akuburass@gmail.com>
2024-03-27 14:00:30 +0200

    Working on the main for now. Just a small amount of work for now. Bit by bit day by day

--- first_try/main.c
+++ first_try/main.c
@@ -6,7 +6,29 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:16:09 by akuburas          #+#    #+#             */
-/*   Updated: 2024/03/27 11:16:10 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/03/27 13:46:29 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	char	*input;
+
+	while (1)
+	{
+		input = readline("shitshell-0.01:");
+		if (!input)
+		{
+			printf("exit");
+			break ;
+		}
+		if (input)
+		{
+			add_history(input);
+		}
+		free(input);
+	}
+	rl_clear_history();
+}
--- first_try/minishell.h
+++ first_try/minishell.h
@@ -6,7 +6,26 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:16:05 by akuburas          #+#    #+#             */
-/*   Updated: 2024/03/27 11:16:06 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/03/27 13:18:15 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* For the readline functions*/
+# include <readline/readline.h>
+# include <readline/history.h>
+
+/* For many such as fork, pipe, access, etc.*/
+# include <unistd.h>
+
+/* For malloc, free, exit, etc.*/
+# include <stdlib.h>
+
+/* For the Open function*/
+# include <fcntl.h>
+
+/*For the signal functions*/
+# include <signal.h>
+#endif
