f32f107c1886cafb99a67cf6021fc22b2cbe5d75
ArminKuburas <akuburass@gmail.com>
2024-04-02 21:47:45 +0300

    Small improvement again. Now we seperate the command out of the input. Next step will be to do the rederections in order. Perhaps create a system that lets us know if something has failed or if a child process should be made? will think about it. Perhaps we can just create ghost children that do nothing but go 'uuuahh I am a ghost'

--- first_try/data_parser.c
+++ first_try/data_parser.c
@@ -6,36 +6,57 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 19:32:05 by akuburas          #+#    #+#             */
-/*   Updated: 2024/04/02 16:06:02 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/04/02 21:29:52 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-int	set_up_data(t_parse_data *parse_data)
+int	set_up_data(t_parse_data *parse_data, char **split_input)
 {
 	int	i;
-	int	save_position;
+	int	process_number;
 
 	i = 0;
-	save_position = 0;
-	while (parse_data->processes > 0)
+	process_number = 0;
+	if (split_input[0][0] != '>' && split_input[0][0] != '<')
 	{
-		while()
+		parse_data->child_data[process_number].command
+			= ft_strdup(split_input[0]);
+		if (parse_data->child_data[process_number].command == NULL)
+			return (NO_MEMORY);
+		i++;
+		if (split_input[i] != NULL && split_input[i][0] == '|')
+			i++;
+		process_number++;
 	}
-	while
+	while (process_number < parse_data->processes)
+	{
+		while (split_input[i] != NULL && split_input[i][0] != '|')
+		{
+			if (split_input[i][0] != '>' && split_input[i][0] != '<' &&
+				split_input[i - 1][0] != '>' && split_input[i - 1][0] != '<')
+			{
+				parse_data->child_data[process_number].command
+					= ft_strdup(split_input[i]);
+				if (parse_data->child_data[process_number].command == NULL)
+					return (NO_MEMORY);
+			}
+			i++;
+		}
+		if (split_input[i] != NULL)
+			i++;
+		process_number++;
+	}
+	process_number = 0;
+	while (process_number < parse_data->processes)
+	{
+		printf("inside print loop\n");
+		printf("Command %d: %s\n", process_number + 1, parse_data->child_data[process_number].command);
+		process_number++;
+	}
+	return (NO_ERROR);
 }
-
-/*Update this note later. First we have to understand what we are parsing and what for.
-First of all we need to make sure we seperate the data properly. Sometimes the data spliting might give us a wrong string
-What I mean by this is for example if an input looks like this: ls -l|echo hello world
-We need to recognize that we should split up -l|echo into -l and echo to be properly used by execve.
-Secondly we need to recognize the special characters we see and also properly order things as needed.
-For example figuring out how to handle things like: ls -l > example1 >> example2 (where does the data go?)
-I have a few ideas here but its very understandable why the parser is so important. We need to have a struct that can handle all this various data
-We will also need a function that properly distrubutes the data within such a structure
-Lastly of course we will need to properly handle any errors beforehand as well since I would rather handle errors before making child processes*/
 int	parse_split_input(t_shelldata *data)
 {
 	int				i;
@@ -53,11 +74,11 @@ int	parse_split_input(t_shelldata *data)
 		}
 		i++;
 	}
-	printf("Inside the parser. This is how many commands there are: %d\n", parse_data.processes);
-	parse_data.child_data = malloc((parse_data.processes) * sizeof(t_child_data));
+	parse_data.child_data
+		= ft_calloc(parse_data.processes + 1, sizeof(t_child_data));
 	if (parse_data.child_data == NULL)
 		return (NO_MEMORY);
-	error = set_up_data(&parse_data);
+	error = set_up_data(&parse_data, data->split_input);
 	if (error != NO_ERROR)
 		return (FAILURE);
 	return (SUCCESS);
--- first_try/main.c
+++ first_try/main.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:16:09 by akuburas          #+#    #+#             */
-/*   Updated: 2024/04/02 10:23:41 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/04/02 21:46:18 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	main(int argc, char **argv, char **env)
 		printf("wtf3\n");
 	while (1)
 	{
-		input = readline("bananashell-0.06:");
+		input = readline("bananashell-0.07:");
 		if (!input)
 		{
 			printf("exit\n");
