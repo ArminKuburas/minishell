3d04dc36c9cb0940a4f2297eb2ab50b02cc63f02
Armin Kuburas <akuburass@gmail.com>
2024-04-05 13:04:08 +0300

    One final commit before merge

--- first_try/minishell.h
+++ first_try/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:16:05 by akuburas          #+#    #+#             */
-/*   Updated: 2024/04/04 18:14:03 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/04/05 12:57:23 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,7 +100,7 @@ typedef struct s_shelldata
 	int				exit_value;
 }		t_shelldata;
 
-//int		rl_clear_history(void);
+int		rl_clear_history(void);
 int		mini_split(char *input, t_shelldata *data);
 void	free_double_array(char ***array);
 int		parse_split_input(t_shelldata *data);
