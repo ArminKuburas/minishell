c60484721449c9a1f3343c738bfa77d9bf1f9a5b
Armin Kuburas <akuburass@gmail.com>
2024-04-19 12:28:21 +0300

    A few slight changes.

--- second_try/minishell.h
+++ second_try/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:16:05 by akuburas          #+#    #+#             */
-/*   Updated: 2024/04/19 04:16:23 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/04/19 10:21:57 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -175,8 +175,8 @@ typedef struct s_split_data
 }	t_split_data;
 //readline functions
 
-//int			rl_clear_history(void);
-//void		rl_replace_line(char *str, int num);
+int			rl_clear_history(void);
+void		rl_replace_line(char *str, int num);
 
 //data_parser functions
 
