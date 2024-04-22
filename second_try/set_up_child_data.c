0135f44ad830926805969bc4db624cb87916120d
Armin Kuburas <akuburass@gmail.com>
2024-04-22 16:06:47 +0300

    feat: working on the function setup_redirects. This function with setup all the redirects requiered.
    This is me testing to see if I can give a body to my commit messages.

--- second_try/set_up_child_data.c
+++ second_try/set_up_child_data.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 11:58:42 by akuburas          #+#    #+#             */
-/*   Updated: 2024/04/22 15:41:47 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/04/22 16:05:35 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,15 @@ void	setup_redirects(t_shelldata *data, int index)
 	}
 	while (temp && temp->type != PIPE)
 	{
-		
+		if (temp->type == REDIRECT_INPUT)
+			handle_redirect_input(&data->child_data[index], temp->next);
+		if (temp->type == REDIRECT_OUTPUT)
+			handle_redirect_output(&data->child_data[index], temp->next);
+		if (temp->type == REDIRECT_APPEND)
+			handle_redirect_append(&data->child_data[index], temp->next);
+		temp = temp->next;
+		if (data->child_data[index].exit_value != 0)
+			return ;
 	}
 }
 
