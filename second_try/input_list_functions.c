db1db6e40d0695e69f75d3ffa98c684f64011e15
Armin Kuburas <akuburass@gmail.com>
2024-05-01 12:16:41 +0300

    fix: only a bit more fixing done.

--- second_try/input_list_functions.c
+++ second_try/input_list_functions.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 15:24:02 by akuburas          #+#    #+#             */
-/*   Updated: 2024/04/19 06:05:33 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/05/01 12:11:59 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,16 +52,16 @@ int	create_input(char *input, int len, t_input_list *input_list)
 	{
 		input_list->input = ft_strndup(input, len);
 		if (input_list->input == NULL)
-			return (clear_input(input_list, NO_MEMORY));
+			return (NO_MEMORY);
 	}
 	else
 	{
 		new_node = (t_input_list *)ft_calloc(1, sizeof(t_input_list));
 		if (new_node == NULL)
-			return (clear_input(input_list, NO_MEMORY));
+			return (NO_MEMORY);
 		new_node->input = ft_strndup(input, len);
 		if (new_node->input == NULL)
-			return (clear_input(input_list, NO_MEMORY));
+			return (NO_MEMORY);
 		add_input_list(input_list, new_node);
 	}
 	return (SUCCESS);
